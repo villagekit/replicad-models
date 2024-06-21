fd11f405fcc8fcce064a452fb413ed983b604408
Michael Williams <hello@mikey.nz>
2024-06-21 23:35:47 +1200

    looking good, except kerf is not accurate

--- models/container.js
+++ models/container.js
@@ -1,54 +1,88 @@
 const defaultParams = {
-  width: 80,
-  depth: 40,
-  boxHeight: 20,
-  backHeight: 20,
-  wallThickness: 1.6,
-  outerFillet: 5,
-  bottomFillet: 5,
+  gridSpacingInMm: 40,
+  widthInGrids: 5,
+  depthInGrids: 2,
+  heightInGrids: 1,
+  fastenerDiameterInMm: 8,
+  wallThicknessInMm: 1.6,
+  outerFilletInMm: 5,
+  bottomFilletInMm: 5,
+  kerfInMm: 1,
 }
 
 /** @typedef { typeof import("replicad") } replicadLib */
 /** @type {function(replicadLib, typeof defaultParams): any} */
 function main(replicad, params) {
-  const { draw } = replicad
-  const { width, depth, boxHeight, backHeight, wallThickness, outerFillet, bottomFillet } = params
+  const { draw, drawCircle } = replicad
+  const {
+    gridSpacingInMm,
+    widthInGrids,
+    depthInGrids,
+    heightInGrids,
+    fastenerDiameterInMm,
+    wallThicknessInMm,
+    outerFilletInMm,
+    bottomFilletInMm,
+    kerfInMm,
+  } = params
+
+  const widthInMm = widthInGrids * gridSpacingInMm - 2 * kerfInMm
+  const depthInMm = depthInGrids * gridSpacingInMm - 2 * kerfInMm
+  const boxHeightInMm = heightInGrids * gridSpacingInMm - kerfInMm
+  const backHeightInMm = gridSpacingInMm - kerfInMm
 
   const base = drawRoundedRectangleWithStraightBack(
     replicad,
-    width - 2 * bottomFillet,
-    depth - 2 * bottomFillet,
-    outerFillet,
+    widthInMm - 2 * bottomFilletInMm,
+    depthInMm - 2 * bottomFilletInMm,
+    outerFilletInMm,
   )
   const wallProfile = draw()
-    .line(bottomFillet, bottomFillet)
-    .customCorner(bottomFillet)
-    .vLine(boxHeight - bottomFillet)
-    .hLine(-wallThickness)
-    .vLine(-boxHeight + wallThickness / 2 + bottomFillet)
-    .customCorner(bottomFillet - wallThickness)
-    .lineTo([0, wallThickness])
+    .line(bottomFilletInMm, bottomFilletInMm)
+    .customCorner(bottomFilletInMm)
+    .vLine(boxHeightInMm - bottomFilletInMm)
+    .hLine(-wallThicknessInMm)
+    .vLine(-boxHeightInMm + wallThicknessInMm / 2 + bottomFilletInMm)
+    .customCorner(bottomFilletInMm - wallThicknessInMm)
+    .lineTo([0, wallThicknessInMm])
     .done()
 
   const box = createProfileBox(replicad, wallProfile, base).translate(
-    -bottomFillet,
-    bottomFillet,
+    -bottomFilletInMm,
+    bottomFilletInMm,
     0,
   )
 
-  const back = drawRoundedRectangleWithStraightBack(replicad, width, backHeight, outerFillet)
+  let back = drawRoundedRectangleWithStraightBack(
+    replicad,
+    widthInMm,
+    backHeightInMm,
+    outerFilletInMm,
+  )
     .sketchOnPlane('XZ')
-    .extrude(wallThickness)
-    .translateY(wallThickness)
-    .translateZ(boxHeight)
+    .extrude(wallThicknessInMm)
+    .translateY(wallThicknessInMm)
+    .translateZ(boxHeightInMm)
+
+  for (const fastenerIndex of range(widthInGrids)) {
+    const fastenerCut = drawCircle((1 / 2) * fastenerDiameterInMm)
+      .sketchOnPlane('XZ')
+      .extrude(wallThicknessInMm)
+      .translate([
+        kerfInMm - (1 / 2 + fastenerIndex) * gridSpacingInMm,
+        wallThicknessInMm,
+        -kerfInMm + (1 / 2 + heightInGrids) * gridSpacingInMm,
+      ])
+    back = back.cut(fastenerCut)
+  }
 
   const shape = box
     .fuse(back)
-    .fillet(wallThickness / 3, (f) =>
+    .fillet(wallThicknessInMm / 3, (f) =>
       f.either([
-        (f) => f.inPlane('XY', boxHeight),
+        (f) => f.inPlane('XY', boxHeightInMm),
         (f) => f.inPlane('XZ'),
-        (f) => f.inPlane('XZ', -wallThickness),
+        (f) => f.inPlane('XZ', -wallThicknessInMm),
       ]),
     )
 
@@ -57,15 +91,15 @@ function main(replicad, params) {
   }
 }
 
-function drawRoundedRectangleWithStraightBack(/** @type replicadLib */ replicad, width, depth, r) {
+function drawRoundedRectangleWithStraightBack(/** @type replicadLib */ replicad, width, height, r) {
   const { draw } = replicad
 
   return draw()
-    .vLine(depth - 2 * r)
+    .vLine(height - 2 * r)
     .tangentArc(-r, r)
     .hLine(-width + 2 * r)
     .tangentArc(-r, -r)
-    .vLine(-depth + 2 * r)
+    .vLine(-height + 2 * r)
     .close()
 }
 
@@ -93,4 +127,8 @@ function createProfileBox(/** @type replicadLib */ replicad, inputProfile, base)
     makeFace(assembleWire(new EdgeFinder().inPlane('XY', end[1]).find(side))),
     baseSketch.face(),
   ])
+}
+
+function range(end) {
+  return Array.from(Array(end).keys())
 }
