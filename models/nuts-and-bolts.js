ba4f9c8bd15fc5f23f74a1ce917934030c208699
Michael Williams <hello@mikey.nz>
2024-07-07 22:27:27 +1200

    hmm..

--- models/nuts-and-bolts.js
+++ models/nuts-and-bolts.js
@@ -9,7 +9,7 @@
 
 export const defaultParams = {
   diameter: 8,
-  length: 20,
+  length: 5,
 }
 
 /**
@@ -42,16 +42,19 @@ function bolt(options) {
     majDiameter,
     pitch,
     threadHeight,
-    length,
+    length: length + pitch,
   })
 
-  return thread
+  const shaft = makeCylinder((1 / 2) * minDiameter, length + 2 * pitch)
+    .rotate(2)
+    .fuse(thread.clone(), {
+      optimisation: 'sameFace',
+    })
+    .translateZ(-pitch)
 
-  const top = makeCylinder((1 / 2) * minDiameter, length)
-    .rotate(2) // to avoid having the end of the thread start on the cylinder seam which breaks the kernel
-    .fuse(thread.clone(), { optimisation: 'sameFace' })
+  const bottom = makeCylinder(majDiameter, pitch).translateZ(-pitch)
 
-  return top
+  return shaft.cut(bottom.clone())
 }
 
 /**
@@ -63,81 +66,24 @@ function bolt(options) {
  * @param {number} options.length
  */
 function isoThread(options) {
-  const {
-    loft,
-    assembleWire,
-    makeLine,
-    makeFace,
-    makeHelix,
-    makeSolid,
-    makePolygon,
-    draw,
-    Plane,
-    Wire,
-    getOC,
-    localGC,
-  } = replicad
+  const { loft, assembleWire, makeHelix, Wire } = replicad
   const { minDiameter, pitch, threadHeight, length } = options
 
-  /** @type {[Replicad.Wire, Replicad.Wire, Replicad.Wire, Replicad.Wire, Replicad.Wire, Replicad.Wire]} */
+  /** @type {[Replicad.Wire, Replicad.Wire, Replicad.Wire, Replicad.Wire]} */
   const helixes = [
-    makeThreadHelix([0, 0]),
     makeThreadHelix([(2 / 16) * pitch, 0]),
     makeThreadHelix([(7 / 16) * pitch, (5 / 8) * threadHeight]),
     makeThreadHelix([(9 / 16) * pitch, (5 / 8) * threadHeight]),
     makeThreadHelix([(14 / 16) * pitch, 0]),
-    makeThreadHelix([(16 / 16) * pitch, 0]),
   ]
 
-  console.log('helixes', helixes)
+  const rootShell = makeRuledSurface(helixes[0], helixes[3])
+  const rootWire = assembleWire(rootShell.wires)
 
-  // console.log('line', makeLine(helixes[0].startPoint, helixes[1].startPoint))
+  const apexShell = makeRuledSurface(helixes[1], helixes[2])
+  const apexWire = assembleWire(apexShell.wires)
 
-  console.log(
-    helixes[0].startPoint.toTuple(),
-    helixes[1].startPoint.toTuple(),
-    helixes[2].startPoint.toTuple(),
-    helixes[3].startPoint.toTuple(),
-    helixes[4].startPoint.toTuple(),
-    helixes[5].startPoint.toTuple(),
-    helixes[0].startPoint.toTuple(),
-  )
-
-  /** @type {[Replicad.Face, Replicad.Face]} */
-  /*
-  const endCapFaces = [
-    makePolygon([
-      helixes[0].startPoint,
-      helixes[1].startPoint,
-      helixes[2].startPoint,
-      helixes[3].startPoint,
-      helixes[4].startPoint,
-      helixes[5].startPoint,
-      helixes[0].startPoint,
-    ]),
-    makePolygon([
-      helixes[0].endPoint,
-      helixes[1].endPoint,
-      helixes[2].endPoint,
-      helixes[3].endPoint,
-      helixes[4].endPoint,
-      helixes[5].endPoint,
-      helixes[0].endPoint,
-    ]),
-  ]
-  */
-
-  const threadFaces = [
-    makeRuledSurface(helixes[0], helixes[1]).faces[0],
-    makeRuledSurface(helixes[1], helixes[2]).faces[0],
-    makeRuledSurface(helixes[2], helixes[3]).faces[0],
-    makeRuledSurface(helixes[3], helixes[4]).faces[0],
-    makeRuledSurface(helixes[4], helixes[5]).faces[0],
-    makeRuledSurface(helixes[5], helixes[0]).faces[0],
-  ]
-
-  // return makeSolid(threadFaces.concat(endCapFaces))
-  return makeSolid(threadFaces)
+  return loft([rootWire, apexWire])
 
   /**
    * @param {[number, number]} offset
