11c49084655b6a39d9746096a6f3723449125e91
Michael Williams <hello@mikey.nz>
2024-10-04 12:20:14 +1300

    cut from solid not profile

--- models/cutting-jig.js
+++ models/cutting-jig.js
@@ -20,23 +20,25 @@ export default function main(params) {
   const lengthInMm = lengthInGu * gridUnitInMm - cutterKerfInMm
   const widthInMm = gridUnitInMm
 
-  let profile = drawRoundedRectangle(lengthInMm, widthInMm, filletInMm).translate([
+  const jigProfile = drawRoundedRectangle(lengthInMm, widthInMm, filletInMm).translate([
     (1 / 2) * lengthInMm + (1 / 2) * cutterKerfInMm,
     0,
   ])
 
+  let jigShape = /** @type Replicad.Solid */ (
+    jigProfile.sketchOnPlane().extrude(heightInMm)
+  ).fillet(filletInMm)
+
   for (let gridIndex = 0; gridIndex < lengthInGu; gridIndex++) {
-    const hole = drawCircle((1 / 2) * holeDiameterInMm).translate([
+    const holeProfile = drawCircle((1 / 2) * holeDiameterInMm).translate([
       (1 / 2 + gridIndex) * gridUnitInMm,
       0,
     ])
 
-    profile = profile.cut(hole)
+    const holeShape = /** @type Replicad.Solid */ (holeProfile.sketchOnPlane().extrude(heightInMm))
+
+    jigShape = jigShape.cut(holeShape)
   }
 
-  const shape = /** @type Replicad.Solid */ (profile.sketchOnPlane().extrude(heightInMm)).fillet(
-    filletInMm,
-  )
-
-  return shape
+  return jigShape
 }
